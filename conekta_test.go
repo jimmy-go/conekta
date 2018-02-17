b16fa04acc63ca3e4f2cd8519ae085488bdd55ac
Angel Del Castillo <jubu201504@gmail.com>
2018-02-16 22:27:38 -0600

    Updated CI.

--- conekta_test.go
+++ conekta_test.go
@@ -1,8 +1,3 @@
-// Package conekta contains tests for Conekta client.
-/*
-	Use of this source code is governed by a MIT
-	license that can be found in the LICENSE file.
-*/
 package conekta
 
 import (
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-go test -v $(go list ./... | grep -v /vendor/)
